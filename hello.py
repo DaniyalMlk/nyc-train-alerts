676452fe01da9506edb9776efcdfb513e62df10b
DaniyalMl <103329154+DaniyalMlk@users.noreply.github.com>
2026-09-16 02:22:16 +0500

    Revert e64035c: Allan's name will be re-added via pull request

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # NYC Train Alerts - TECH-UB 24 team repo
 # Each member adds their name below via their own pull request.
 
-team = ["Daniyal", "Walter Kusuma", "Allan You"]
+team = ["Daniyal", "Walter Kusuma",]
 
 print("Hello from the NYC Train Alerts team!")
 print("Team members: " + ", ".join(team))
