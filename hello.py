58b99795764e8296b03bb8d6a6861ac28bdd19e9
DaniyalMl <103329154+DaniyalMlk@users.noreply.github.com>
2026-09-16 01:46:24 +0500

    Add hello-world starter

---
+++ hello.py
@@ -0,0 +1,8 @@
+# NYC Train Alerts - TECH-UB 24 team repo
+# Each member adds their name below via their own pull request.
+
+team = [
+]
+
+print("Hello from the NYC Train Alerts team!")
+print("Team members: " + ", ".join(team))
